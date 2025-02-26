9b768d92a3c19428c07207db764857bde8442651
Legatus <152823324+Legatus2326@users.noreply.github.com>
2025-02-26 18:37:10 -0300

    Criando o Back-End do cadastro de funcionários

---
+++ Back-End [Cadastro Funcionário]/FuncionarioDTO.java
@@ -0,0 +1,110 @@
+package DTO;
+
+public class FuncionarioDTO {
+
+    private String nome_funcionario, cpf_funcionario, email_funcionario, telefone_funcionario, cargo, senha_funcionario;
+    private int id_funcionario;
+    /**
+     * @return the id_funcionario
+     */
+    public int getId_funcionario() {
+        return id_funcionario;
+    }
+
+    /**
+     * @param id_funcionario the id_funcionario to set
+     */
+    public void setId_funcionario(int id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+   
+
+    /**
+     * @return the nome_funcionario
+     */
+    public String getNome_funcionario() {
+        return nome_funcionario;
+    }
+
+    /**
+     * @param nome_funcionario the nome_funcionario to set
+     */
+    public void setNome_funcionario(String nome_funcionario) {
+        this.nome_funcionario = nome_funcionario;
+    }
+
+    /**
+     * @return the cpf_funcionario
+     */
+    public String getCpf_funcionario() {
+        return cpf_funcionario;
+    }
+
+    /**
+     * @param cpf_funcionario the cpf_funcionario to set
+     */
+    public void setCpf_funcionario(String cpf_funcionario) {
+        this.cpf_funcionario = cpf_funcionario;
+    }
+
+    /**
+     * @return the email_funcionario
+     */
+    public String getEmail_funcionario() {
+        return email_funcionario;
+    }
+
+    /**
+     * @param email_funcionario the email_funcionario to set
+     */
+    public void setEmail_funcionario(String email_funcionario) {
+        this.email_funcionario = email_funcionario;
+    }
+
+    /**
+     * @return the telefone_funcionario
+     */
+    public String getTelefone_funcionario() {
+        return telefone_funcionario;
+    }
+
+    /**
+     * @param telefone_funcionario the telefone_funcionario to set
+     */
+    public void setTelefone_funcionario(String telefone_funcionario) {
+        this.telefone_funcionario = telefone_funcionario;
+    }
+
+    /**
+     * @return the cargo_funcionario
+     */
+    public String getCargo() {
+        return cargo;
+    }
+
+    /**
+     * @param cargo_funcionario the cargo_funcionario to set
+     */
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    /**
+     * @return the senha
+     */
+
+    /**
+     * @return the senha_funcionario
+     */
+    public String getSenha_funcionario() {
+        return senha_funcionario;
+    }
+
+    /**
+     * @param senha_funcionario the senha_funcionario to set
+     */
+    public void setSenha_funcionario(String senha_funcionario) {
+        this.senha_funcionario = senha_funcionario;
+    }
+    
+}
